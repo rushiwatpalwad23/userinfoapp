3e33781ad5cf4e000046213a77578878ef2ddc14
Rushikesh Watpalwad <Rushikesh Watpalwad@TSLPUNE.LOCAL>
2024-10-21 16:29:32 +0530

    second commit

--- config.py
+++ config.py
@@ -5,9 +5,9 @@ class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'your_secret_key_here')
 
     # MySQL Database URI (update with your credentials)
-    MYSQL_USER = os.getenv('MYSQL_USER', 'root')
-    MYSQL_PASSWORD = os.getenv('MYSQL_PASSWORD', 'root')
-    MYSQL_HOST = os.getenv('MYSQL_HOST', 'localhost')  # for localhost use 'localhost'
+    MYSQL_USER = os.getenv('MYSQL_USER', 'admin')
+    MYSQL_PASSWORD = os.getenv('MYSQL_PASSWORD', 'Rutuja*1998')
+    MYSQL_HOST = os.getenv('MYSQL_HOST', 'database-1.czma46y8s5sd.ap-south-1.rds.amazonaws.com')  # for localhost use 'localhost'
     MYSQL_DB = os.getenv('MYSQL_DB', 'userinfoapp')
 
     SQLALCHEMY_DATABASE_URI = f'mysql+mysqlconnector://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}/{MYSQL_DB}'
